18ae1c997cc49de7d4b65e7fe83d9112d2374925
kamal14rajput <kamal14rajput@gmail.com>
2022-01-19 00:01:51 +0530

    UPDATED: multiple rides

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -2,12 +2,17 @@ package com.bridgelabz;
 
 public class CabInvoiceGenerator {
 	public final int COST_PER_KILOMETER = 10;
-	private final int COST_PER_MINUTE = 1;
+    private final int COST_PER_MINUTE = 1;
 
-	public int calculateFare(int distance, int time) {
-		int totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
-		if (totalFare < 5)
-			totalFare = 5;
-		return totalFare;
-	}
+    public int calculateFare(int distance, int time) {
+        int totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        if(totalFare < 5) totalFare = 5;
+        return totalFare;
+    }
+    public int calculateTotalAggregateFare(Ride[] rides) {
+        int totalAggregateFare = 0;
+        for(Ride ride : rides)
+            totalAggregateFare += calculateFare(ride.distance , ride.time);
+        return totalAggregateFare;
+    }
 }
