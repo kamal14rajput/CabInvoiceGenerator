f0d075478126353f8254c4ea0c7092440e294f94
kamal14rajput <kamal14rajput@gmail.com>
2022-01-19 00:24:20 +0530

    UPDATED: list of rides and returns invoice

--- src/main/java/com/bridgelabz/Ride.java
+++ src/main/java/com/bridgelabz/Ride.java
@@ -2,10 +2,10 @@ package com.bridgelabz;
 
 public class Ride {
 	public double distance;
-    public int time;
+	public int time;
 
-    public Ride(double distance, int time) {
-        this.distance = distance;
-        this.time = time;
-    }
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
 }
