2ccce052d02b0de831b533c89e9de763f13cd379
kamal14rajput <kamal14rajput@gmail.com>
2022-01-18 23:54:35 +0530

    ADDED: test for fare calculation

---
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.CabInvoiceGenerator;
+
+public class CabInvoiceGeneratorTest {
+
+	 @Test
+	    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		 CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+	        int distance = 10, time = 12;
+	        int totalFare = cabInvoiceGenerator.calculateFare(distance, time);
+	        Assert.assertEquals(112, totalFare);
+	    }
+}
