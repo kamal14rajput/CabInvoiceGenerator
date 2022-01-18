27c649b2bc89accf5328e7511ce1ddb2b455feeb
kamal14rajput <kamal14rajput@gmail.com>
2022-01-19 00:04:01 +0530

    UPDATED: test for multiple rides

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -4,14 +4,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.CabInvoiceGenerator;
+import com.bridgelabz.Ride;
 
 public class CabInvoiceGeneratorTest {
 
 	 @Test
 	    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		 CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-	        int distance = 10, time = 12;
-	        int totalFare = cabInvoiceGenerator.calculateFare(distance, time);
-	        Assert.assertEquals(112, totalFare);
+		    int distance = 10, time = 12;
+		    Assert.assertEquals(112, new CabInvoiceGenerator().calculateFare(distance, time));
+	    }
+	 @Test
+	    public void givenMultipleRides_ShouldReturnTotalFare() {
+	        Ride[] rides = { new Ride(10, 15), new Ride(3, 8), new Ride(4, 10) };
+	        Assert.assertEquals(203, new CabInvoiceGenerator().calculateTotalAggregateFare(rides));
 	    }
 }
